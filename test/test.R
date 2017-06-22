663bf9dd8cf3351f27786b2cee273f2e9c245477
phoenixding <phoenixjunding@gmail.com>
2017-06-22 13:46:12 -0400

    rmethrao test example

---
+++ test/test.R
@@ -0,0 +1,15 @@
+library("rmethrafo")
+# download reference genome
+#rmethrafo::download("hg19","hg19")
+
+# process the bam file (bigWig from bam)
+rmethrafo::bamScript("example_medip.bam","hg19/hg19.chrom.sizes")
+
+
+# predict based on the bigwig file from bam
+rmethrafo::predict("hg19","example_medip.bam.sort.bam.bedGraph.sort.bw", paste(path.package("rmethrafo"),"model/rr.pkl",sep="/"),"e1_out")
+
+# predict based on the bigwig file directly
+rmethrafo::predict("hg19","example_medip.bw", paste(path.package("rmethrafo"),"model/rr.pkl",sep="/"),"e2_out")
+
+
